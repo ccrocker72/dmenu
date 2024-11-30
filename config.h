8e3cf4d0b32a9a5af2ded91985493bf66eca8816
Christopher Crocker <chris.crocker7227@gmail.com>
2024-11-30 11:43:02 -0500

    custom config changes

--- config.h
+++ config.h
@@ -6,13 +6,13 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=16"
+	"monospace:size=26"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	[SchemeSel] = { "#ebdbb2", "#928374" },
+	[SchemeSel] = { "#ebdbb2", "#458588" },
 	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
